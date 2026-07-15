67f111ef9754dcb78a33703ac7789db12b7a2bd0
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2026-07-15 03:45:22 +0300

    feat: set up the enviroment

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { AppEnvironment } from "../app/core/tokens/environment.token";
+
+export const environment: AppEnvironment = {
+  production: true,
+  apiUrl: "http://localhost:8080/api",
+};
